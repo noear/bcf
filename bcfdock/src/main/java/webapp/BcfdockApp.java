2f2919c9e5d73de5c28da5f33ad144d98e4ad040
noear <noear@live.cn>
2021-08-31 10:46:05 +0800

    2.1.5

--- bcfdock/src/main/java/webapp/BcfdockApp.java
+++ bcfdock/src/main/java/webapp/BcfdockApp.java
@@ -1,21 +1,29 @@
 package webapp;
 
+import org.noear.snack.ONode;
 import org.noear.solon.Solon;
 import org.noear.solon.core.handle.Context;
+import org.noear.solon.logging.utils.TagsMDC;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class BcfdockApp {
 
     public static void main(String[] args) {
-        Logger logger = LoggerFactory.getLogger("water_log_admin");
+        Logger log = LoggerFactory.getLogger("water_log_admin");
 
         Solon.start(BcfdockApp.class, args).onError((err) -> {
             Context ctx = Context.current();
+
             if (ctx == null) {
-                logger.error("bcfdock", "", err);
+                log.error("< Error: {}", err);
             } else {
-                logger.error("bcfdock", ctx.path(), err);
+                TagsMDC.tag1(ctx.pathNew());
+
+                log.error("> Header: {}\n> Param: {}\n\n< Error: {}",
+                        ONode.stringify(ctx.headerMap()),
+                        ONode.stringify(ctx.paramMap()),
+                        ctx.errors);
             }
         });
     }
